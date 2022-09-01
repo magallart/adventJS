cf067a9729bf63cdc7d1f3dd6d5365e394725b5c
Miguel Ángel Gallart <magallart58@gmail.com>
2022-09-01 19:13:23 +0200

    ejercicio 2 resuelto

---
+++ 02/02.js
@@ -0,0 +1,23 @@
+const carta = 'bici coche balón _playstation bici coche peluche';
+
+function listGifts(letter) {
+  const giftCount = {};
+
+  const giftsFiltered = letter
+    .trim()
+    .split(' ')
+    .filter((gift) => !gift.startsWith('_'));
+
+  giftsFiltered.forEach((gift) => {
+    if (giftCount[gift]) {
+      giftCount[gift]++;
+      return;
+    }
+    giftCount[gift] = 1;
+  });
+
+  return giftCount;
+}
+
+const regalos = listGifts(carta);
+console.log(regalos);
