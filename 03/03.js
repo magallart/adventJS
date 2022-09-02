0a4475651599486e7b6321b7800ac433181f7183
Miguel Ángel Gallart <magallart58@gmail.com>
2022-09-02 12:52:01 +0200

    ejercicio 03 resuelto

---
+++ 03/03.js
@@ -0,0 +1,20 @@
+const string1 = 'bici coche (balón bici) coche peluche';
+const string2 = '(muñeca) consola bici';
+const string3 = 'bici coche (balón bici coche';
+const string4 = 'peluche (bici [coche) bici coche balón';
+const string5 = '(peluche {) bici';
+const string6 = '(()) bici';
+const string7 = '(a() bici " debería ser inválido';
+
+function isValid(letter) {
+
+  const regEx1 = new RegExp(/\[|\]|\{|\}/); // Contiene {,},[,]
+  const regEx2 = new RegExp(/\(\)/); // Contiene ()
+  const regEx3 = new RegExp(/\(*[A-Z|a-z|0-9]\)/); // () estén llenos
+  
+  if (regEx1.test(letter)) return false;
+  if (regEx2.test(letter)) return false;
+  return regEx3.test(letter);
+}
+
+console.log(isValid(string1));
